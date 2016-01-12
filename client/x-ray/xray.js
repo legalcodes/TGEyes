ba1277d3ed4ce0fbd1b6f038c79c6ae34bb31401
Jon Tippens <jwtippens@gmail.com>
2016-01-11 20:10:00 -0800

    Installed xray, selected posts on test blog

---
+++ client/x-ray/xray.js
@@ -0,0 +1,33 @@
+var Xray = require('x-ray');
+
+var xray = new Xray();
+
+// xray('https://ksiddana.github.io/', 'h1', [{
+//   h1: ''
+// }])
+//   .write('results.json');
+
+xray('https://ksiddana.github.io/', '.post', [{
+  title: 'a', 
+  text: 'p', 
+  url: xray('a', '@href')
+
+}])
+  .write('results.json');
+
+
+
+
+
+
+/*
+
+xray(html, '.item', [{
+  title: 'h2',
+  tags: x('.tags', ['li'])
+}])(function(err, arr) {
+  console.log(arr);
+});
+
+
+*/
