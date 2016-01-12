13bbd2cf7400e934f3edb13776c3b2049fd78242
Jon Tippens <jwtippens@gmail.com>
2016-01-11 17:06:02 -0800

    Implemented some styling with Twitter Bootstrap

--- client/posts.js
+++ client/posts.js
@@ -1,13 +1,35 @@
-var TGEyes = angular.module('TGEyes', []);
+var TGEyes = angular.module('TGEyes', ["ui.router"]);
+TGEyes.config(function($stateProvider, $urlRouterProvider){
+  //send unmatched urls to blogList
+  $urlRouterProvider.otherwise("/viewBlogs")
+
+  $stateProvider
+    .state('viewBlogs', {
+      url: "/viewBlogs", 
+      templateUrl: "viewBlogs.html",
+      controller: "postController"
+    })
+    .state('addBlog', {
+      url: "/addBlog", 
+      templateUrl: "addBlog.html",
+      controller: "postController"
+    })
+})
 
 TGEyes.controller('postController', ['$scope', 'PostFactory', function($scope, PostFactory){
-  $scope.data = ["!"];
-  $scope.display = "Scope display is working!";
+  
+
+  $scope.data = [];
+
+
+  $scope.addText = "SCOPE DISPLAY WORKING";
+  $scope.viewText = "ADD BLOG IS WORKING";
   
   $scope.fetchPosts = function(){
     PostFactory.getPosts()
       .then(function(data){
         $scope.data = data;
+        console.log($scope.data);
         console.log('\n\n\nfetchedPosts from controller!!!!!\n\n\n');
       })
   }
@@ -19,10 +41,10 @@ TGEyes.controller('postController', ['$scope', 'PostFactory', function($scope, P
 
 TGEyes.factory('PostFactory', ['$http', function($http){
   return {
-    getPosts:function(){
+    getPosts: function(){
       return $http.get('http://localhost:3000/data').then(function(res){
         console.log(res);
-        return res;
+        return res.data;
       });
     }
   }
